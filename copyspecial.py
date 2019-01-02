ac3d6b6a949689659a5184da0b5a942619c4482c
David Stewart <davidstewy@gmail.com>
2019-01-02 12:44:13 -0500

    adding check if a path exists

--- copyspecial.py
+++ copyspecial.py
@@ -27,6 +27,9 @@ def get_special_paths(directory):
 
 
 def copy_to(paths, directory):
+    directory = '.' + directory
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     for path in paths:
         shutil.copy(path, directory)
 
